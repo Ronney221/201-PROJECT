4b6033add07dac5f45ea7849ba236d7e67ac0361
ronney221 <ronneydo1@gmail.com>
2017-12-07 02:16:08 -0800

    fixed justins pi chart into new tab

--- ui.R
+++ ui.R
@@ -45,22 +45,11 @@ my.ui <- fluidPage(
           h3("SCATTERPLOT:"),
           p("This scatterplot will compare the favorite count and viewer counts of the 1000 selected animes. The radio buttons on the sidepanel allow for the scatterplot to be filtered by type of entertainment, because viewer and favorite counts for different types of productions have different numbers."),
           h3("EXPLANATION OF THE GRAPH:"),
-          p("This graph represents the actual satisfaction rate for the people who have watched the anime. On the X-axis, we sort the animes by ascending view count, while on the y-axis we sort by the favorite count divided by the viewer count. This allows us to see the percentage of people who watched the anime and genuinely enjoyed it."),
-          radioButtons("showTypes", "Overview of Showtypes or Specific Anime",
-                       choices = c("Overview", "Specific")),
-          selectInput("specificAnime", "Choose an Anime",
-                      choices = c("Neon Genesis Evangelion", 
-                                  "Rurouni Kenshin: Meiji Kenkaku Romantan",
-                                  "Mobile Suit Gundam", "Cowboy Bebop",
-                                  "El Hazard", "Hunter x Hunter",
-                                  "Initial D", "Love Hina"), 
-                      selected = "Neon Genesis Evangelion")
-          
+          p("This graph represents the actual satisfaction rate for the people who have watched the anime. On the X-axis, we sort the animes by ascending view count, while on the y-axis we sort by the favorite count divided by the viewer count. This allows us to see the percentage of people who watched the anime and genuinely enjoyed it.")
         ),
         
         mainPanel(
-          plotlyOutput("scatterplot"),
-          plotlyOutput("pie")
+          plotlyOutput("scatterplot")
         )
       )
     ),
@@ -80,6 +69,7 @@ my.ui <- fluidPage(
                       selected = "Neon Genesis Evangelion")
         ),
         mainPanel(
+          plotlyOutput("pie")
         )
       )
     ),
